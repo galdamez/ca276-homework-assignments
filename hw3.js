bb701a49eeef1dda3dc56a23193cafe7eb73da1f
Jose Galdamez <mcstudent@jose-galdamezs-computer.local>
2013-09-24 08:21:28 -0400

    Added Homework 3

---
+++ hw3.js
@@ -0,0 +1,59 @@
+/*
+ * Please follow the instructions carefully.
+ * 
+ * When a problem says "send 'foo' to the console that means you would write
+ * 
+ * 	console.log('foo');
+ * 
+ * I may also say "send the variable x to the console" in which case you'd write
+ * 
+ * 	console.log(x)
+ * 
+ * Note: If you have multiple variables x, y, z you can send all of them to the console like so
+ * 
+ * 	console.log(x, y, z)
+ */
+
+// Problem 1
+// Send the following calculations to the console.
+// 5 plus 5
+// 5 minus 5
+// 5 times 5
+// 5 divided by 5
+
+
+
+// Problem 2
+	// Write a series of if/else if statements that produce the following responses to the console.
+	// If “Dad” then “He’s going to be in trouble!”
+	// If “Mom” then “No big deal.”
+	// If “Me” then “No more cookies for you.”
+	// If none of the above, “Johnny did it.”
+
+
+// Problem 3
+	// Look at the three Boolean conditions in the sentence below.
+
+	// “If the day is Thursday, the time is ‘6:30 PM’ and the room is ‘HU 317’ then you are in CA276. Otherwise, you are in another class.”
+
+	// Express this sentence as an “if” statement with the proper comparison (=== or !==) and Boolean (&& or ||) operators. You will send one of the following sentences to the console.
+
+	// “You are in CA276”
+	// “You are in another class”
+
+
+// Problem 4
+	// Create a variable bartSimpson and set it equal to the empty string.
+	// 
+	// var bartSimpson = '';
+	// 
+	// Write a while loop that repeatedly concatenates the string below to bartSimpson: 
+	// 
+	// 'I will not make fun of the teacher using JavaScript code.'
+	// 
+	// Your loop will do this 10 times. Send only the final version of the string to the console. Do not output the string during each loop iteration.
+	// Your final result will be the equivalent of console.log('I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code.');
+
+
+// Problem 5
+	// Do the same as you did in question 4 but write it as a for loop. A sample for loop can be found below.
