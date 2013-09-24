8e66eb99c583dfcd84cf1019f2b4e24931c1dd91
Jose Galdamez <mcstudent@jose-galdamezs-computer.local>
2013-09-24 08:29:15 -0400

    Homework 3 edits

--- hw3.js
+++ hw3.js
@@ -14,6 +14,7 @@
  * 	console.log(x, y, z)
  */
 
+
 // Problem 1
 // Send the following calculations to the console.
 // 5 plus 5
@@ -24,20 +25,29 @@
 
 
 // Problem 2
-	// Write a series of if/else if statements that produce the following responses to the console.
+	// Write a series of conditions that send one of the following responses to the console.
+	// 
 	// If “Dad” then “He’s going to be in trouble!”
 	// If “Mom” then “No big deal.”
 	// If “Me” then “No more cookies for you.”
 	// If none of the above, “Johnny did it.”
+	// 
+	// Start by defining a variable cookieThief and assigning it one of the values above.
+	// 
+	// var cookieThief = 'something';
+	// 
+	// You can accomplish this with a series of if, if else, if else if, or a function that has return statements. Choose whichever you think works best.
+	// 
+	// You can review Rebecca Murphey's post on how to avoid using "else if" by going here: http://rmurphey.com/blog/2012/12/10/js-conditionals/
 
 
 // Problem 3
 	// Look at the three Boolean conditions in the sentence below.
-
+	// 
 	// “If the day is Thursday, the time is ‘6:30 PM’ and the room is ‘HU 317’ then you are in CA276. Otherwise, you are in another class.”
-
+	// 
 	// Express this sentence as an “if” statement with the proper comparison (=== or !==) and Boolean (&& or ||) operators. You will send one of the following sentences to the console.
-
+	// 
 	// “You are in CA276”
 	// “You are in another class”
 
@@ -47,12 +57,19 @@
 	// 
 	// var bartSimpson = '';
 	// 
-	// Write a while loop that repeatedly concatenates the string below to bartSimpson: 
+	// Write a while loop that repeatedly concatenates the string below to the variable bartSimpson: 
 	// 
 	// 'I will not make fun of the teacher using JavaScript code.'
 	// 
 	// Your loop will do this 10 times. Send only the final version of the string to the console. Do not output the string during each loop iteration.
-	// Your final result will be the equivalent of console.log('I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code.');
+	// 
+	// Your final result will be the equivalent of saying
+	// 
+	// console.log('I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code. I will not make fun of the teacher using JavaScript code.');
+	// 
+	// only you'll be writing 
+	// 
+	// console.log(bartSimpson);
 
 
 // Problem 5
