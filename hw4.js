decf08a59b490dd2d2fdace102f02e2d3f9578ee
Jose Galdamez <mcstudent@jose-galdamezs-computer.local>
2013-10-18 07:58:18 -0400

    Homework 4 comments

--- hw4.js
+++ hw4.js
@@ -16,7 +16,7 @@
 
 // Methods (i.e. functions): 
 
-// introduce = returns 'Hello! My name is _______ and I am from ________'
+// introduce = returns 'Hello! My name is _______ and I am from ________'. Fill in the blanks with the values name and country of origin, respectively.
 
 // addMedal = This takes a string as a parameter and adds it to the array of medals. It does not return anything.
 
@@ -60,24 +60,29 @@ var athlete3 = {
 // https://github.com/galdamez/ca276-fall2013/blob/master/week8/prototypes.html
 
 
+
 // PROBLEM BELOW IS OPTIONAL
 
 // Problem 5
-// Create an prototype that inherits from Athlete. Pick from any of the types below:
+// Create a prototype that inherits from Athlete. Pick from any of the types below:
 // TennisPlayer
 // TableTennisPlayer
 // Swimmer
 // Sprinter
 // …or make your own!
 // 
-// This new prototype definition will add one method that returns a string message.
+// This new prototype definition will add one method that returns a string message. It will also add a property unique to this type of athlete.
 // 
-// You get to decide what those are.
+// You get to decide how to define these.
 // 
 // For example I may want to have a Swimmer type that does the following.
 // 
-// Parameters in order are: name, age, country, height (in meters), medals, is he an Olympian?
-// var s1 = new Swimmer('Michael Phelps', 28, 'USA', 1.93 ['gold', 'gold', 'silver'], true);
+// Parameters in order are: name, age, country, height (in meters), medals, is he an Olympian?, preferred brand
+// 
+// var s1 = new Swimmer('Michael Phelps', 28, 'USA', 1.93 ['gold', 'gold', 'silver'], true, 'Speedo');
+// 
+// The last property, preferred brand, is the one I added.
 // 
 // Method below returns 'Splash splash splash!'
+// 
 // s1.swim();
